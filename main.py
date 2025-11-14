c7e2ac29307eb697756650ff06e7261e56a3b4b6
Tomson247906 <damian.cardnoa@gmail.com>
2025-11-14 02:20:52 +0000

    getting into level 3

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from library_books import library_books
 from datetime import datetime, timedelta
 
+
+
 # -------- Level 1 --------
 # TODO: Create a function to view all books that are currently available
 # Output should include book ID, title, and author
+
+
+#This allows the user to view all available books in the library
+def view_available_books():
+    print("Available Books:")
+    for book in library_books:
+        if book['available']:
+            print(f"ID: {book['id']}, Title: {book['title']}, Author: {book['author']}")
 
 
 # -------- Level 2 --------
@@ -11,6 +21,16 @@ from datetime import datetime, timedelta
 # Search should be case-insensitive
 # Return a list of matching books
 
+#allows the user to search for books by author or genre and to see if its id, title, author, genre and if its available
+def search_by_author_or_genre(search_term):
+    search_term = search_term.strip().lower()
+    matching_books = []
+    for book in library_books:
+        author = book.get('author','').lower()
+        genre = book.get('genre','').lower()
+        if search_term in author or search_term in genre:
+            matching_books.append(book)
+    return matching_books
 
 # -------- Level 3 --------
 # TODO: Create a function to checkout a book by ID
@@ -21,6 +41,19 @@ from datetime import datetime, timedelta
 # If it is not available:
 #   - Print a message saying it's already checked out
 
+#This function allows the user to check out a book by its ID and lets the
+def check_out_book(book_id):
+    for book in library_books:
+        if book["id"] == book_id:
+            if book['available']:
+                book["available"] = False
+                book["due_date"] = datetime.now() + timedelta(weeks=2)
+                book['checkouts'] = book.get('checkouts', 0) + 1
+                print(f"You have successfully checked out '{book['title']}'. It is due on {book['due_date'].strftime('%Y-%m-%d')}.")
+            else:
+                print(f"Sorry, '{book['title']}' is already checked out.")
+            return
+    print("Book ID not found.")
 
 # -------- Level 4 --------
 # TODO: Create a function to return a book by ID
@@ -34,6 +67,37 @@ from datetime import datetime, timedelta
 # TODO: Convert your data into a Book class with methods like checkout() and return_book()
 # TODO: Add a simple menu that allows the user to choose different options like view, search, checkout, return, etc.
 
+# This function provides a simple text-based menu for the library system
+#def guide():
+    while True:
+        print("\n welcome to the library inventory")
+        print("1: view available books")
+        print("2: search by genre or author")
+        print("3: check out")
+        print("4: check in")
+        print("5: close program")
+
+        choice = input("pick an opition(1-5): ")
+
+        if choice == "1":
+            print("viewing books")
+            view_available_books()
+        elif choice == "2":
+            search = input("searching by genre or author")
+            
+            
+        elif choice == "3":
+            print("working progress")
+        elif choice == "4":
+            print("working progress")
+        elif choice == "5":
+            print("now closing")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+
+
 # -------- Optional Advanced Features --------
 # You can implement these to move into Tier 4:
 # - Add a new book (via input) to the catalog
@@ -45,3 +109,9 @@ from datetime import datetime, timedelta
 if __name__ == "__main__":
     # You can use this space to test your functions
     pass
+
+
+print(check_out_book("B1"))
+#print(search_by_author_or_genre("Rick Riordan"))
+#view_available_books()
+#guide()
